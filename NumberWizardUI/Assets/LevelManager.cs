a77cb2034e6fd1bf0adb2e3c61f3361a56f8c967
Jenny Olsen <jenny.a.olsen@gmail.com>
2017-03-02 14:50:43 -0800

    number wizard ui, switch between scenes

---
+++ NumberWizardUI/Assets/LevelManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+	public void LoadLevel (string name)
+	{
+		Debug.Log ("level load requested for " + name);
+		Application.LoadLevel (name);
+	}
+}
