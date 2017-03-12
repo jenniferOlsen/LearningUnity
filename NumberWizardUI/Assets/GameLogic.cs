25026e6de0b16c8fd885ac8242b98d7491c55d26
Jenny Olsen <jenny.a.olsen@gmail.com>
2017-03-11 17:31:32 -0800

    add ui to Number Wizard

---
+++ NumberWizardUI/Assets/GameLogic.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour
+{
+
+	// Use this for initialization
+	int min;
+	int max;
+	int guess;
+	int maxGuessesAllowed = 5;
+	
+	public Text currentGuess;
+	
+	void Start ()
+	{
+		StartGame ();
+	}
+	
+	void StartGame ()
+	{
+		max = 1001;
+		min = 1;
+		NextGuess ();
+		
+	}
+	
+	public void GuessHigher ()
+	{
+		min = guess;
+		NextGuess ();
+	}
+	
+	public void GuessLower ()
+	{
+		max = guess;
+		NextGuess ();
+	}
+	
+	
+	void NextGuess ()
+	{
+		guess = Random.Range (min, max);
+		currentGuess.text = guess.ToString ();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed < 0) {
+			Application.LoadLevel ("Win");
+		}
+	}
+	
+	
+}
